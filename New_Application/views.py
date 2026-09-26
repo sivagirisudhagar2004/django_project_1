bd5f8a8d096b15585174132059b5fb363e686061
sivagirisudhagar2004 <sivagirisudhagar@gmail.com>
2026-09-26 18:25:32 +0530

    Changes

--- New_Application/views.py
+++ New_Application/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+"""""
 def home(request):
     return render(request,"home.html")
 
@@ -13,4 +13,3 @@ def register(resquest):
 """""
 def home(request):
     return render(request,"home_1.html")
-"""""
--- django_project_1/urls.py
+++ django_project_1/urls.py
@@ -20,6 +20,6 @@ from New_Application import views
 
 urlpatterns = [
     path('',views.home),
-    path('register',views.register)
+    
     
 ]
